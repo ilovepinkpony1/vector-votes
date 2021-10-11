9553700bb285fb3a10670b36290fd53850aae783
Vitalii <vitalii_oliinik>
2021-10-11 18:49:03 +0300

    feat: added basic structure

---
+++ app.js
@@ -0,0 +1,131 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
+import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-database.js"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7jJYYkMFR9bCiYkQfHFNynUhVMp7oInU",
+  authDomain: "vector-54c00.firebaseapp.com",
+  projectId: "vector-54c00",
+  storageBucket: "vector-54c00.appspot.com",
+  messagingSenderId: "559110889150",
+  appId: "1:559110889150:web:535316ecae3f1331d588ef",
+  databaseURL: "https://vector-54c00-default-rtdb.europe-west1.firebasedatabase.app",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+
+
+const buttons = document.querySelectorAll('.buttonWrapper button')
+const buttonsWrappers = document.querySelectorAll('.buttonWrapper')
+const countWrapper = document.querySelector('.votesCountWrapper')
+const votesCountWrapper = document.querySelector('.votesCount')
+const isVoted = getCookie('user-vector-voted')
+const votesData = {}
+
+const getActualVotes = async () => {
+  try {
+    const snapshot = await get(ref(db, 'votes'))
+
+    if (snapshot.exists()) {
+      let allVotesCount = 0
+
+      Object.entries(snapshot.val()).forEach(([snapKey, snapValue]) => {
+        if (snapKey && snapValue && snapValue.votes) {
+          votesData[snapKey] = snapValue.votes
+          allVotesCount += snapValue.votes
+        }
+      })
+
+      votesCountWrapper.innerHTML = allVotesCount
+
+      if (isVoted) {
+        countWrapper.classList.add('votesCountWrapperVisible')
+      }
+
+    } 
+  } catch(err) {
+    console.error(error);
+  }
+}
+
+const onButtonClick = async (teamType) => {
+  const date = new Date()
+  date.setDate(date.getDate() + 100);
+
+  try {
+
+    await getActualVotes()
+
+    let votes = 1
+    if (votesData[teamType]) {
+      votes += votesData[teamType]
+    }
+
+    votesData[teamType] = votes
+
+    set(ref(db, 'votes/' + teamType), {
+      votes,
+    });
+
+    
+    votesCountWrapper.innerHTML = (parseInt(votesCountWrapper.innerText) || 0) + 1
+
+    setCookie('user-vector-voted', true, {
+      expires: date
+    })
+
+    countWrapper.classList.add('votesCountWrapperVisible')
+    buttonsWrappers.forEach(wrapper => {
+      wrapper.classList.add('disabledButtonWrapper')
+    })
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+window.addEventListener('load', () => {
+  if (isVoted) {
+    getActualVotes()
+    buttonsWrappers.forEach(wrapper => {
+      wrapper.classList.add('disabledButtonWrapper')
+    })
+  } else {
+    buttons.forEach(button => {
+      const teamType = button.dataset.team
+      button.addEventListener('click', () => {
+        onButtonClick(teamType)
+      })
+    })
+  }
+})
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options = {}) {
+  options = {
+    path: '/',
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
